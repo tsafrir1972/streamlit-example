1e66b921de6fd64bd2132a42128641421ae8c5ac
tsafrir1972 <98154639+tsafrir1972@users.noreply.github.com>
2022-09-18 14:38:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -262,7 +262,234 @@ class Flights():
         st.table(df_airports)
         #print(df_list_airports)
         return df_list_airports        
-        
+
+class Hotels():
+
+    def find_hotels(self, city):
+
+        global df_hotels
+        global df_hotels_string
+
+        if city == 'New York':
+            city = "60763"
+        elif city == 'bangkok':
+            city = "301643"
+        elif city == 'Barcelona':
+            city = "1465497"
+
+        url = "https://travel-advisor.p.rapidapi.com/airports/search"
+
+        uri = 'mongodb+srv://tsafrir:tsafrir@cluster0.frf1eeg.mongodb.net/?retryWrites=true&w=majority'
+        myclient = pymongo.MongoClient(uri)
+        mydb = myclient["travel_app"]
+        mycol = mydb["Hotels"]
+
+        x = mycol.delete_many({})
+
+        client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+        url = "https://travel-advisor.p.rapidapi.com/hotels/list"
+
+        querystring = {"location_id": city, "adults": "1", "rooms": "1", "nights": "2", "offset": "0",
+                       "currency": "USD", "order": "asc", "limit": "10", "sort": "recommended", "lang": "en_US"}
+
+        headers = {
+            'X-RapidAPI-Key': 'aedd40ee0emsh7abd403f44557b7p1236b2jsn91e7e9c7abf6',
+            "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com"
+        }
+
+
+
+        response_hotels = requests.request("GET", url, headers=headers, params=querystring)
+
+        dict_json = json.loads(response_hotels.text)
+
+        try:
+
+                #print(' dict json :',dict_json)
+
+                json_length = len(dict_json)
+
+                #print('hotels_list_length :', json_length)
+
+                for list_json in dict_json.values():
+
+                    list_length = len(list_json)
+
+
+                    try:
+                        for i in range(0, list_length):
+                            try:
+                                # print(f"The value for your request is {list_json[i]}")
+                                None
+                                var_price = random.randint(50, 150)
+                                var_flight_number = random.randint(10000, 15000)
+
+                                list_json[i]['price_in_dollars'] = var_price
+
+                                mycol.insert_one(list_json[i])
+                            except KeyError:
+                                None
+                                # print(f"There is no parameter with the '{list_json[i]}' key. ")
+                    except:
+                        None
+
+
+                df_hotels = pd.DataFrame(list(mycol.find()))
+
+                #df_hotels = df_hotels.reset_index(inplace=True)
+
+                additional_cols = ['price_in_dollars']
+
+                df_hotels = df_hotels.reindex(df_hotels.columns.tolist(), axis=1)
+
+                df_hotels = df_hotels.applymap(str)
+
+                df_hotels = df_hotels.loc[:, ["name", "hotel_class", "price_in_dollars"]]
+
+                df_hotels = df_hotels.loc[df_hotels["price_in_dollars"].apply(pd.to_numeric) <= int(selected_hotel_budget)]
+
+                # df_hotels['hotel_class'] = df_hotels['hotel_class'].astype('int')
+
+                df_hotels.sort_values(by=['hotel_class'], ascending=False).head(3)
+
+                df_hotels = df_hotels.head(3)
+
+                df_hotels_string = df_hotels.to_string()
+
+                def cell_colours(series):
+                    red = 'background-color: red;'
+                    yellow = 'background-color: yellow;'
+                    turquoise = 'background-color: turquoise;'
+                    default = ''
+
+                    return [red if data == "failed" else yellow if data == "error" else green if data == "passed"
+                    else turquoise for data in series]
+
+                headers = {
+                    'selector': 'th.col_heading',
+                    'props': 'background-color: #000066; color: white;'
+                }
+                df_hotels = df_hotels.style.set_table_styles([headers]) \
+                    .apply(cell_colours)
+
+                st.table(df_hotels)
+
+        except Exception:
+                df_hotels_string = 'Hotels API didn`t return results'
+                st.write('Hotels API didn`t return results')
+
+class Restorants():
+
+    def find_restorants(self, city):
+
+        global df_restorants
+        global df_restorants_string
+
+        if city == 'New York':
+            city = "60763"
+        elif city == 'bangkok':
+            city = "301643"
+        elif city == 'Barcelona':
+            city = "1465497"
+
+        uri = 'mongodb+srv://tsafrir:tsafrir@cluster0.frf1eeg.mongodb.net/?retryWrites=true&w=majority'
+        myclient = pymongo.MongoClient(uri)
+        mydb = myclient["travel_app"]
+        mycol = mydb["Restorants"]
+
+        x = mycol.delete_many({})
+
+        client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+        url = "https://travel-advisor.p.rapidapi.com/restaurants/list"
+
+        querystring = {"location_id": city, "restaurant_tagcategory": "10591",
+                       "open_now": "false", "lang": "en_US"}
+
+        headers = {
+            'X-RapidAPI-Key': 'aedd40ee0emsh7abd403f44557b7p1236b2jsn91e7e9c7abf6',
+            "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com"
+        }
+
+
+
+        response_restorants = requests.request("GET", url, headers=headers, params=querystring)
+
+        dict_json = json.loads(response_restorants.text)
+
+        try:
+
+                for list_json in dict_json.values():
+                    # print(list_json)
+                    list_length = len(list_json)
+
+                    # print(list_length)
+                    try:
+                        for i in range(0, list_length):
+                            try:
+                                # print(f"The value for your request is {list_json[i]}")
+                                None
+                                var_price = random.randint(50, 150)
+                                var_flight_number = random.randint(10000, 15000)
+                                list_json[i]['price_in_dollars'] = var_price
+
+                                mycol.insert_one(list_json[i])
+                            except KeyError:
+                                None
+                                # print(f"There is no parameter with the '{list_json[i]}' key. ")
+                    except:
+                        None
+
+                df_restorants = pd.DataFrame(list(mycol.find()))
+
+                df_restorants = df_restorants.applymap(str)
+
+                df_restorants = df_restorants.loc[:, ["name", "rating", "price_in_dollars"]]
+
+                df_restorants = df_restorants.loc[
+                    df_restorants["price_in_dollars"].apply(pd.to_numeric) <= int(selected_restorants_budget)]
+
+                # df_restorants['rating'] = df_restorants['rating'].astype('int')
+
+                df_restorants.sort_values(by=['rating'], ascending=False).head(3)
+
+                df_restorants = df_restorants.head(3)
+
+                df_restorants_string = df_restorants.to_string()
+
+               # print(df_restorants)
+
+                def cell_colours(series):
+                    red = 'background-color: red;'
+                    yellow = 'background-color: yellow;'
+                    turquoise = 'background-color: turquoise;'
+                    default = ''
+
+                    return [red if data == "failed" else yellow if data == "error" else green if data == "passed"
+                    else turquoise for data in series]
+
+                headers = {
+                    'selector': 'th.col_heading',
+                    'props': 'background-color: #000066; color: white;'
+                }
+                df_restorants = df_restorants.style.set_table_styles([headers]) \
+                    .apply(cell_colours)
+
+                st.table(df_restorants)
+
+                df = pd.DataFrame(
+                    np.random.randn(1000, 2) / [50, 50] + [40.69, -74],
+                    columns=['lat', 'lon'])
+
+                st.map(df)
+
+        except Exception:
+                df_restorants_string = 'Restorants API didn`t return Results'
+                TravelOptions().send_to_user_email(user_email)
+                st.write('Restorants API didn`t return Results')    
+    
+    
         
 if __name__ == '__main__':
 
