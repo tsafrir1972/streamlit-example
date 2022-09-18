475f75b86c5cc1a9a592ac8934f6715bba9c14e7
tsafrir1972 <98154639+tsafrir1972@users.noreply.github.com>
2022-09-18 09:11:09 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,17 +3,9 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import random
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 
 
 class TravelOptions:
