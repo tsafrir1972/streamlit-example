b5613d9b65749454b4fce6969ad52a891c1bd500
tsafrir1972 <98154639+tsafrir1972@users.noreply.github.com>
2022-09-18 09:03:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,10 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+if __name__ == '__main__':
+
+    TravelOptions()
+
 class TravelOptions:
 
     def __init__(self):
